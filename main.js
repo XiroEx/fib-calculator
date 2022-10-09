d4b1340417d8e4af60c2e04927b6d9f8909185a5
XiroEx <george8794@gmail.com>
2022-10-09 17:22:48 -0400

    added export

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 
 import * as Fib from 'fib-retracement'
 import redis from 'redis'
-
-async function fibs() {
+async function fibs(asset, time) {
     const Publisher = redis.createClient({url:'redis://:jaD4z4Yz2INBVIDsWXr5TZolIQpQAhEI@redis-16236.c238.us-central1-2.gce.cloud.redislabs.com:16236'})
     await Publisher.connect()
-    const candles = (await Publisher.zRange('SOL-PERP-5mftx', 0, -1)).map(c=>(JSON.parse(c)))
+    const candles = (await Publisher.zRange(`${asset}-${time}ftx`, 0, -1)).map(c=>(JSON.parse(c)))
     const high = candles.reduce((a,o) => (a ? Math.max(a.value,o[2]) == a.value ? a : {value: o[2], time: o[0]} : {value: o[2], time: o[0]}), 0)
     const low = candles.reduce((a,o) => (a ? Math.min(a.value,o[3]) == a.value ? a : {value: o[3], time: o[0]} : {value: o[3], time: o[0]}), 0)
     console.log(high.time > low.time ? 'Retracement (Uptrend, Buy Targets)' : 'Extension (Downtrend, Sell Targets)')
@@ -16,7 +15,9 @@ async function fibs() {
     } else {
         console.log('Data Error')
     }
-    console.log(await Publisher.get('SOL-PERP-ftx'))
+    console.log(`Price : ${await Publisher.get('SOL-PERP-ftx')}`)
+    Publisher.disconnect()
 }
-fibs()
+fibs('SOL-PERP', '15m') //!TEMP
 
+export default fibs
