16ed1f01b4d8de31f039db3f2ded39c8cd100b69
George Abreu Jr <george8794@gmail.com>
2022-12-14 15:26:50 -0500

    replaced ftx as data source with bitfinex & alpaca

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ import redis from 'redis'
 async function fibs(asset, time) {
     const Publisher = redis.createClient({url:'redis://:jaD4z4Yz2INBVIDsWXr5TZolIQpQAhEI@redis-16236.c238.us-central1-2.gce.cloud.redislabs.com:16236'})
     await Publisher.connect()
-    const candles = (await Publisher.zRange(`${asset}-${time}ftx`, 0, -1)).map(c=>(JSON.parse(c)))
+    const exchange = asset.includes('/') || asset.includes('-') ? 'bitfinex':'alpaca'
+    const candles = (await Publisher.zRange(`candles:${exchange}:${asset.replace('/',':')}:${time}`, 0, -1)).map(c=>(JSON.parse(c)))
     const high = candles.reduce((a,o) => (a ? Math.max(a.value,o[2]) == a.value ? a : {value: o[2], time: o[0]} : {value: o[2], time: o[0]}), 0)
     const low = candles.reduce((a,o) => (a ? Math.min(a.value,o[3]) == a.value ? a : {value: o[3], time: o[0]} : {value: o[3], time: o[0]}), 0)
     let message = {m:`${high.time > low.time ? 'Retracement (Uptrend, Buy Targets)' : 'Extension (Downtrend, Sell Targets)'}`}
@@ -15,10 +16,10 @@ async function fibs(asset, time) {
     } else {
         message.f = 'Data Error'
     }
-    message.p = await Publisher.get('SOL-PERP-ftx')
+    message.p = await Publisher.get(`price:${exchange}:${asset.replace('/',':')}`)
     Publisher.disconnect()
     return (message)
 }
-//console.log(await fibs('SOL-PERP', '1h')) //!TEMP
+//console.log(await fibs('NVDA', '15m')) //!TEMP
 
 export default fibs
